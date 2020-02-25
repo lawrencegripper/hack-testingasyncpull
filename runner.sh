73e1cc7c0c0e1595ed59ea0a877784d75bf54f27
Lawrence Gripper <info@grippers.co.uk>
2020-02-25 07:30:23 +0000

    Results with dockerhub throttle

--- runner.sh
+++ runner.sh
@@ -2,27 +2,43 @@
 
 set -e
 
+iteration=1
+
 run () {
+    iteration=$((iteration+1))
     POD_NAME=$1
     RESULTS_DIR=results-$POD_NAME
 
     mkdir -p $RESULTS_DIR
 
+    # Cleanup images
+    docker image rm -f lawrencegripper/big:200mb || true
+    docker image rm -f lawrencegripper/big:600mb || true
+    docker image rm -f lawrencegripper/big:1000mb || true
+
     kind delete cluster --name control || true
+    
     kind create cluster --name control --image kindest/node:v1.17.0
     JSONPATH='{range .items[*]}{@.metadata.name}:{range @.status.conditions[*]}{@.type}={@.status};{end}{end}'; until kubectl get nodes -o jsonpath="$JSONPATH" 2>&1 | grep -q "Ready=True"; do sleep 5; echo "--------> waiting for cluster node to be available"; done
     kubectl apply -f ./single-init/$POD_NAME.yaml
     # Wait for pod to complete
     until kubectl get pod -o json | jq ".items[].status.phase" | grep -q "Succeeded"; do sleep 5;echo "--------> waiting for pod to complete"; done
+    kubectl describe pod $POD_NAME > ./$RESULTS_DIR/normal-$POD_NAME-$iteration.json
     kubectl get events --field-selector involvedObject.name==$POD_NAME -o json | jq '[.items[] | {"reason": .reason, "message": .message, "time": .firstTimestamp }]'  > ./$RESULTS_DIR/normal-$POD_NAME.json
-
+    
     kind delete cluster --name control || true
-    kind create cluster --name control --image kindest/node:lawrence
+
+    kind delete cluster --name lg || true
+
+    kind create cluster --name lg --image kindest/node:lawrence
     JSONPATH='{range .items[*]}{@.metadata.name}:{range @.status.conditions[*]}{@.type}={@.status};{end}{end}'; until kubectl get nodes -o jsonpath="$JSONPATH" 2>&1 | grep -q "Ready=True"; do sleep 5; echo "--------> waiting for cluster node to be available"; done
     kubectl apply -f ./single-init/$POD_NAME.yaml
     # Wait for pod to complete
     until kubectl get pod -o json | jq ".items[].status.phase" | grep -q "Succeeded"; do sleep 5;echo "--------> waiting for pod to complete"; done
+    kubectl describe pod $POD_NAME > ./$RESULTS_DIR/async-$POD_NAME-$iteration.json
     kubectl get events --field-selector involvedObject.name==$POD_NAME -o json | jq '[.items[] | {"reason": .reason, "message": .message, "time": .firstTimestamp }]'  > ./$RESULTS_DIR/async-$POD_NAME.json
+    
+    kind delete cluster --name lg || true
 
 
     echo "RESULTS: Before"
@@ -35,12 +51,14 @@ run () {
 run control
 run control
 
+sleep 300
 run 200mb-10sec
 run 200mb-10sec
 
+sleep 300
 run 600mb-10sec
 run 600mb-10sec
 
+sleep 300
 run 1000mb-10sec
 run 1000mb-10sec
-
